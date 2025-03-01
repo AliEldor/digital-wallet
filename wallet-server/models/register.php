7274444cc8387cc2cce2122c9f5579cd55cc5fdd
Ali Eldor <alieldor02@gmail.com>
2025-03-01 23:28:43 +0200

    check if there is an error in the inputs and prepare the stmt to execute the query

--- wallet-server/models/register.php
+++ wallet-server/models/register.php
@@ -16,4 +16,20 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])
     $passwordHash = password_hash($password, PASSWORD_DEFAULT);
     $errors = [];
 
+    if (empty($fullName) || empty($email) || empty($password)) {
+        $errors[] = "All fields are required.";
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Invalid email.";
+    }
+    if (strlen($password) < 5) {
+        $errors[] = "Password must be at least 5 characters.";
+    }
+
+    $sql = "SELECT * FROM users WHERE email =?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $email);
+    mysqli_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
     
