37a5758964b02a6d655560b17f09a0e647e7bc54
Ali Eldor <alieldor02@gmail.com>
2025-03-01 23:03:21 +0200

    create response array and check if form data was sent

--- wallet-server/models/register.php
+++ wallet-server/models/register.php
@@ -3,4 +3,17 @@
 include("../connection/connection.php");
 header('Content-Type: application/json');
 
+$response = [
+    "success" => false,
+    "message" => "",
+    "errors" => []
+];
 
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])) {
+    $fullName = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+    $errors = [];
+
+    
