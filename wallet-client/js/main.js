13d33b8f6668bc8b7f5e0535e582a675fd47afe3
Ali Eldor <alieldor02@gmail.com>
2025-03-02 00:35:50 +0200

    check is form exists, add event listener and create formData object

--- wallet-client/js/main.js
+++ wallet-client/js/main.js
@@ -1,20 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const registerForm = document.getElementById("register-form");
 
-document.addEventListener('DOMContentLoaded',function(){
-    const registerForm  =document.getElementById('register-form');
+  if (registerForm) {
+    registerForm.addEventListener("submit", function (event) {
+      event.preventDefault();
 
-    if(registerForm){
-        registerForm.addEventListener('submit', function(event){
-            event.preventDefault();
+      const formData = new FormData(registerForm);
 
-            // get form values
-            const name = form.querySelector('input[name="name"]').value;
-            const email = form.querySelector('input[name="email"]').value;
-            const password = form.querySelector('input[name="password"]').value;
-        })
-
-        
-    }
-
-
-
-})
+      
