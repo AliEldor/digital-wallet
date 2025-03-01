bb156879960354e3c45e3e4a80e345a1f36a210f
Ali Eldor <alieldor02@gmail.com>
2025-03-01 17:38:37 +0200

    get data from form

--- wallet-client/js/main.js
+++ wallet-client/js/main.js
@@ -1,5 +1,20 @@
 
 document.addEventListener('DOMContentLoaded',function(){
-    const form =document.getElementById('register-form');
+    const registerForm  =document.getElementById('register-form');
+
+    if(registerForm){
+        registerForm.addEventListener('submit', function(event){
+            event.preventDefault();
+
+            // get form values
+            const name = form.querySelector('input[name="name"]').value;
+            const email = form.querySelector('input[name="email"]').value;
+            const password = form.querySelector('input[name="password"]').value;
+        })
+
+        
+    }
+
+
 
 })
