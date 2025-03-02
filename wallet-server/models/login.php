dfa804efad941b2775b28cf1c71b176f6aa23fb4
Ali Eldor <alieldor02@gmail.com>
2025-03-02 16:02:16 +0200

    add sql query and prepared statement

--- wallet-server/models/login.php
+++ wallet-server/models/login.php
@@ -13,6 +13,11 @@ if(isset($_POST['login'])){
     $password = $_POST['password'];
 
     $sql = "SELECT email,password FROM users WHERE email=?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "ss", $email,$password);
+    mysqli_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
     
 }
 
