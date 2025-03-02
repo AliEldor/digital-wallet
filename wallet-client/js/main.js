c4f892d89005356c84918e9d9c9426bd26397d15
Ali Eldor <alieldor02@gmail.com>
2025-03-02 16:41:07 +0200

    add functions to make code reusable

--- wallet-client/js/main.js
+++ wallet-client/js/main.js
@@ -1,31 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const registerForm = document.getElementById("register-form");
+    const registerForm = document.getElementById("register-form");
+    const loginForm = document.getElementById("login-form");
+  
+    // Register 
+    if (registerForm) {
+      registerForm.addEventListener("submit", handleRegister);
+    }
 
-  if (registerForm) {
-    registerForm.addEventListener("submit", function (event) {
+    // LOgin
+    if(loginForm){
+      loginForm.addEventListener("submit", handleLogin);
+    }
+  });
+
+    function handleRegister(event){
       event.preventDefault();
-
-      const formData = new FormData(registerForm);
+      const formData = new FormData(event.target);
 
       axios
-        .post("../../wallet-server/models/register.php", formData)
-        .then(function (response) {
-          if (response.data.success) {
-            alert("Registration successful!");
-            window.location.href = "../index.html";
-          } else {
-            if (response.data.errors && response.data.errors.length > 0) {
-              const errorMessage = response.data.errors.join("\n");
-              alert(errorMessage);
+          .post("../../wallet-server/models/register.php", formData)
+          .then(function (response) {
+            if (response.data.success) {
+              alert("Registration successful!");
+              window.location.href = "../index.html";
             } else {
-              alert(response.data.message || "Registration failed");
+                if (response.data.errors && response.data.errors.length > 0) {
+                    const errorMessage = response.data.errors.join("\n");
+                    alert(errorMessage);  
+                  } else {
+                    alert(response.data.message || "Registration failed");  // General message if no specific errors
+                  }
             }
-          }
-        })
-        .catch(function (error) {
-          alert("Connection error. Please try again later.");
-          console.log(error);
-        });
-    });
-  }
-});
+          })
+          .catch(function (error) {
+            alert("Connection error. Please try again later.");
+            console.log(error);
+          });
+      };
+    
+      
+
+  
