4366e184f665c1a5606bf76ebd77743922227621
Ali Eldor <alieldor02@gmail.com>
2025-03-03 22:12:42 +0200

    create event listeners for hamburger menu and profile picture

---
+++ wallet-client/js/dashboard.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let subMenu = document.getElementById("subMenu");
+  let menuIcon = document.getElementById("menu-icon");
+  let navLinks = document.querySelector(".nav-links");
+
+  document.querySelector(".profile-pic").addEventListener("click", (event) => {
+    event.stopPropagation();
+    subMenu.classList.toggle("open-menu");
+  });
+
+  menuIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+  });
+
+
+
+
+  document.addEventListener("click", (event) => {
+    if (
+      !subMenu.contains(event.target) &&
+      !event.target.matches(".profile-pic")
+    ) {
+      subMenu.classList.remove("open-menu");
+    }
+  });
+
+  document.addEventListener("click", (event) => {
+    if (
+      !navLinks.contains(event.target) &&
+      !event.target.matches(".menu-icon")&&
+      !menuIcon.contains(event.target)
+    ) {
+      navLinks.classList.remove("open");
+    }
+  });
+
+  
+
+  
+});
