cb398d099ee05a039c2816d35abb13bf1b9ae06a
Ali Eldor <alieldor02@gmail.com>
2025-03-05 09:16:43 +0200

    if current user id exists, fetch user ids except the current one

--- wallet-server/user/v1/getUsers.php
+++ wallet-server/user/v1/getUsers.php
@@ -8,3 +8,20 @@ $response = [
     "message" => "unable to fetch users",
     "errors" => []
 ];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['currentUserId'])) {
+        $currentUserId = intval($_POST['currentUserId']);
+    } else {
+        $userId = 0;
+    }
+
+    if ($currentUserId > 0) {
+
+        $sql = "SELECT id, full_name FROM users WHERE id != ?";
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "i", $currentUserId);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+    
+}
