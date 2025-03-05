8bb9037f24dc8afa47e395e1fb64dd31feb9d4bd
Ali Eldor <alieldor02@gmail.com>
2025-03-05 23:12:49 +0200

    check for userid and amount values

--- wallet-server/user/v1/withdraw.php
+++ wallet-server/user/v1/withdraw.php
@@ -8,3 +8,16 @@ $response = [
     "message" => "Invalid request",
     "errors" => []
 ];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $userId = isset($_POST['userId']) ? intval($_POST['userId']) : 0;
+    $amount = isset($_POST['amount']) ? floatval($_POST['amount']) : 0;
+
+    if ($userId <= 0) {
+        $response["message"] = "Invalid user ID";
+        echo json_encode($response);
+        exit();
+    }
+
+
+}
