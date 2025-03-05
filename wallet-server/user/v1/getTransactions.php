1cc82b2e8ad39557dfd692ca180a77e99c682e0d
Ali Eldor <alieldor02@gmail.com>
2025-03-05 19:47:13 +0200

    select last 10 transactions

--- wallet-server/user/v1/getTransactions.php
+++ wallet-server/user/v1/getTransactions.php
@@ -9,3 +9,21 @@ $response = [
     "transactions" => []
 ];
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['userId'])) {
+        $userId = intval($_POST['userId']);
+
+        $sql = "SELECT 
+                    transaction_type, 
+                    amount, 
+                    created_at
+                FROM 
+                    transactions
+                WHERE 
+                    sender_id = ? OR recipient_id = ?
+                ORDER BY 
+                    created_at DESC
+                LIMIT 10";
+
+    }
+}
