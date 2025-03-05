ca53454967368ff5e427c3dabeefa0a384a2aeb7
Ali Eldor <alieldor02@gmail.com>
2025-03-05 06:23:20 +0200

    prepare the sql to display the new balance

--- wallet-server/user/v1/addMoney.php
+++ wallet-server/user/v1/addMoney.php
@@ -29,6 +29,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         mysqli_stmt_bind_param($stmt, "di", $amount, $userId);
         
 
+        if (mysqli_stmt_execute($stmt)) {
+
+            // if there is time left could call it from getBalance
+            
+            $balance_sql = "SELECT balance FROM wallets WHERE user_id = ?";
+            $balance_stmt = mysqli_prepare($conn, $balance_sql);
+            mysqli_stmt_bind_param($balance_stmt, "i", $userId);
+            mysqli_stmt_execute($balance_stmt);
+            $balance_result = mysqli_stmt_get_result($balance_stmt);
+            $balance_row = mysqli_fetch_assoc($balance_result);
+
+            $response["success"] = true;
+            $response["message"] = "Money added successfully";
+            $response["new_balance"] = $balance_row['balance'];
+        } else {
+            $response["message"] = "Failed to add money";
+        }
+    } else {
+        $response["message"] = "Invalid user ID or amount";
     }
 }
 
