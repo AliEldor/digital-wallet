c5fc886c177dc73ad17cef0f2ddad8a341da35b0
Ali Eldor <alieldor02@gmail.com>
2025-03-05 06:15:59 +0200

    check the request, and check if userID is in the request, then transform it to int and same for amount

--- wallet-server/user/v1/addMoney.php
+++ wallet-server/user/v1/addMoney.php
@@ -9,3 +9,21 @@ $response = [
     "errors" => []
 ];
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['userId'])) {
+        $userId = intval($_POST['userId']);
+    } else {
+        $userId = 0;
+    }
+
+    if (isset($_POST['amount'])) {
+        $amount = floatval($_POST['amount']);
+    } else {
+        $amount = 0;
+    }
+
+    
+}
+
+echo json_encode($response);
+exit();
