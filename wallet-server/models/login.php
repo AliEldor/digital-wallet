b1fbe20b807cbb5430993598b0590c8428cd6e4b
Ali Eldor <alieldor02@gmail.com>
2025-03-05 04:48:56 +0200

    check if user has a wallet,if not create one

--- wallet-server/models/login.php
+++ wallet-server/models/login.php
@@ -39,12 +39,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if ($user) {
             
             if (password_verify($password, $user["password"])) {
-    
+
+                //check if the user has a wallet
+
+                $wallet_sql = "SELECT * FROM wallets WHERE user_id = ?";
+                $wallet_stmt = mysqli_prepare($conn, $wallet_sql);
+                mysqli_stmt_bind_param($wallet_stmt, "i", $user['id']);
+                mysqli_stmt_execute($wallet_stmt);
+                $wallet_result = mysqli_stmt_get_result($wallet_stmt);
+
+                // if not 
+
+                if (mysqli_num_rows($wallet_result) == 0) {
+                    $create_wallet_sql = "INSERT INTO wallets (user_id, balance, created_date, `last updated`) VALUES (?, 0, NOW(), NOW())";
+                    $create_wallet_stmt = mysqli_prepare($conn, $create_wallet_sql);
+                    mysqli_stmt_bind_param($create_wallet_stmt, "i", $user['id']);
+                    mysqli_stmt_execute($create_wallet_stmt);
+                }
+
+
+
+
+
+                $response["success"] = true;
                 $response["user"] = "yes";
                 $response["user_id"] = $user["id"];
                 $response["user_name"] = $user["full_name"];
 
-                $response["success"] = true;
+                
               
             } else {
                 
